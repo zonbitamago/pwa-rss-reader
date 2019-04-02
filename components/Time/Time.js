ab492b5ae91139db5ec162a7b3e653834b70ba35
kazuma takiuchi <basstrombone413@gmail.com>
2019-04-03 04:54:31 +0900

    add timer process

--- components/Time/Time.js
+++ components/Time/Time.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
 import "./Time.css";
+import dayjs from "dayjs";
 
 const Time = () => {
+  const getMMDD = () => {
+    return dayjs().format("M/D");
+  };
+  const getHHmmss = () => {
+    return dayjs().format("HH:mm:ss");
+  };
+
+  const [MMDD, setMMDD] = useState(getMMDD());
+  const [HHmmss, setHHmmss] = useState(getHHmmss());
+
+  const setTime = () => {
+    setMMDD(getMMDD());
+    setHHmmss(getHHmmss());
+  };
+  setInterval(setTime, 1000);
+
   return (
     <div className="Time">
-      {/* <div className="Time-MMDD">{this.state.MMDD}</div>
-      <div className="Time-HHmmss">{this.state.HHmmss}</div> */}
-      <div>Time</div>
+      <div className="Time-MMDD">{MMDD}</div>
+      <div className="Time-HHmmss">{HHmmss}</div>
     </div>
   );
 };
