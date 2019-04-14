18748516041741e70cb91f625d57441e9bddd210
kazuma takiuchi <basstrombone413@gmail.com>
2019-04-15 04:52:36 +0900

    add fetchitem test

--- store/ItemStore.js
+++ store/ItemStore.js
@@ -2,10 +2,24 @@ import { action, observable } from "mobx";
 
 class ItemStore {
   @observable updateDuration = 5;
+  @observable items = [];
 
   @action.bound
   setUpdateDuration(updateDuration) {
     this.updateDuration = updateDuration;
+  }
+
+  @action.bound
+  fetchItems() {
+    const item = {
+      alt: "alt",
+      src: "https://www.google.com/s2/favicons?domain=qiita.com",
+      domainName: "Qiita",
+      date: "2019/04/13",
+      url: "https://qiita.com/zonbitamago/items/4e215e305062dde016bb",
+      itemName: "無料で爆速なWebアプリケーションを作ろう！"
+    };
+    this.items.push(item);
   }
 }
 
--- store/ItemStore.test.js
+++ store/ItemStore.test.js
@@ -1,21 +1,47 @@
 import ItemStore from "./ItemStore";
 
+let store;
+
+beforeEach(() => {
+  store = new ItemStore();
+});
+
 describe("ItemStore", function() {
   it("Init", async () => {
-    const store = new ItemStore();
     expect(store.updateDuration).toBe(5);
+    expect(store.items.length).toBe(0);
   });
 
   describe("setUpdateDuration", function() {
-    it("updateDuration to 4", async () => {
-      const store = new ItemStore();
-      store.setUpdateDuration(4);
-      expect(store.updateDuration).toBe(4);
-    });
-    it("updateDuration to 6", async () => {
-      const store = new ItemStore();
-      store.setUpdateDuration(6);
-      expect(store.updateDuration).toBe(6);
+    const cases = [
+      ["updateDuration to 4", 4, 4],
+      ["updateDuration to 6", 6, 6]
+    ];
+    describe.each(cases)(
+      "updateDuration",
+      (title, updateDuration, expected) => {
+        it(title, async () => {
+          store.setUpdateDuration(updateDuration);
+          expect(store.updateDuration).toBe(expected);
+        });
+      }
+    );
+  });
+
+  describe("fetchItems", function() {
+    it("fetchItemsFromRss", async () => {
+      store.fetchItems();
+      const item = store.items[0];
+      expect(item.alt).toBe("alt");
+      expect(item.src).toBe(
+        "https://www.google.com/s2/favicons?domain=qiita.com"
+      );
+      expect(item.domainName).toBe("Qiita");
+      expect(item.date).toBe("2019/04/13");
+      expect(item.url).toBe(
+        "https://qiita.com/zonbitamago/items/4e215e305062dde016bb"
+      );
+      expect(item.itemName).toBe("無料で爆速なWebアプリケーションを作ろう！");
     });
   });
 });
