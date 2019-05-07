1fb6ee169b7eb5277bf2d6ae46eb2e8fb95e5a53
kazuma takiuchi <basstrombone413@gmail.com>
2019-05-08 04:56:05 +0900

    change fetch process to parallel

--- store/ItemStore.ts
+++ store/ItemStore.ts
@@ -2,6 +2,7 @@ import { action, observable } from "mobx";
 import FetchRssFeed from "../utils/FetchRssFeed";
 import * as DayFormatter from "../utils/DayFormatter";
 import * as LocalStorageManager from "../utils/LocalStorageManager";
+import * as ArrayUtils from "../utils/ArrayUtils";
 
 export interface ItemElementInterface {
   alt: string;
@@ -35,55 +36,68 @@ class ItemStore {
 
     this.isLoading = true;
 
-    const urls = feedList.map(elem => {
-      return elem.url;
-    });
-    const json = await FetchRssFeed(urls);
-    json.results.forEach(node => {
-      const src: string =
-        "https://www.google.com/s2/favicons?domain=" +
-        node.feed.link.split("//")[1];
+    // const urls = feedList.map(elem => {
+    //   return elem.url;
+    // });
+    // const json = await FetchRssFeed(urls);
 
-      const domainName = feedList.filter(elem => {
-        return elem.url == node.url;
-      })[0].name;
-
-      node.feed.items.forEach(element => {
-        const itemName: string = element.title;
-        const url: string = element.link;
-        const date: string = DayFormatter.YYYYMMDDHH24MMSS(
-          element.publishedParsed
-            ? element.publishedParsed
-            : element.updatedParsed
-        );
-        const item: ItemElementInterface = {
-          alt: "alt",
-          src: src,
-          domainName: domainName,
-          date: date,
-          url: url,
-          itemName: itemName
-        };
-        this.saveItems.push(item);
+    const promiseList = ArrayUtils.splitArrayGroups(feedList, 5).map(groups => {
+      const urls = groups.map(elem => {
+        return elem.url;
       });
+
+      return FetchRssFeed(urls);
     });
 
-    this.saveItems = this.saveItems.slice().sort((a, b) => {
-      if (a.date < b.date) {
-        return 1;
-      }
-      if (a.date > b.date) {
-        return -1;
-      }
+    return Promise.all(promiseList).then(groups => {
+      groups.forEach(json => {
+        json.results.forEach(node => {
+          const src: string =
+            "https://www.google.com/s2/favicons?domain=" +
+            node.feed.link.split("//")[1];
 
-      // names must be equal
-      return 0;
+          const domainName = feedList.filter(elem => {
+            return elem.url == node.url;
+          })[0].name;
+
+          node.feed.items.forEach(element => {
+            const itemName: string = element.title;
+            const url: string = element.link;
+            const date: string = DayFormatter.YYYYMMDDHH24MMSS(
+              element.publishedParsed
+                ? element.publishedParsed
+                : element.updatedParsed
+            );
+            const item: ItemElementInterface = {
+              alt: "alt",
+              src: src,
+              domainName: domainName,
+              date: date,
+              url: url,
+              itemName: itemName
+            };
+            this.saveItems.push(item);
+          });
+        });
+      });
+
+      this.saveItems = this.saveItems.slice().sort((a, b) => {
+        if (a.date < b.date) {
+          return 1;
+        }
+        if (a.date > b.date) {
+          return -1;
+        }
+
+        // names must be equal
+        return 0;
+      });
+
+      this.items = this.saveItems;
+      this.saveItems = [];
+      this.isLoading = false;
+      this.updateTime = DayFormatter.HH24MMSS();
     });
-
-    this.items = this.saveItems;
-    this.saveItems = [];
-    this.isLoading = false;
-    this.updateTime = DayFormatter.HH24MMSS();
   }
 
   @action.bound
