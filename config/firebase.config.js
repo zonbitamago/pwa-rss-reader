4cce53dd205746ac60cb03fa1da96f34c983cd7e
kazuma takiuchi <basstrombone413@gmail.com>
2019-05-20 04:49:41 +0900

    add dotenv

---
+++ config/firebase.config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv/config";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
